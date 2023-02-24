f46fd90d4ea7bc6853106f5943da9843f09a815d
Namanskshetty <namanskshetty@gmail.com>
2023-02-24 11:43:19 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Home } from "./Pages/Home";
 import { Login } from "./Pages/Login";
 import { Register } from "./Pages/Register";
 import "./Style.scss";
 function App() {
   return (
- <Login/>
+ <Home/>
   );
 }
 
